a7c2c241a9cac9b32166fb4615ed93c51923213d
Mateus-Az <mateo.azv3@gmail.com>
2025-05-22 21:34:12 -0300

    create project

---
+++ src/main/java/br/com/vortexlab/VortexLab/VortexLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.vortexlab.VortexLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vortexlab/VortexLab/VortexLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vortexlab.VortexLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
